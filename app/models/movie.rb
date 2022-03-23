2f448c04ed254ada7f282fc859b6f0429909753d
TLCurd <TLCurdChiro@gmail.com>
2022-03-22 21:34:23 -0600

    adds associations for movie/actors

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,5 @@ class Movie < ApplicationRecord
   validates :title, presence: true
   validates :year, length: {is: 4}
   validates :plot, length: { minimum: 10 }
+  has_many :actors
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,14 @@
 require "ffaker"
 
-10.times do 
-  actor = Actor.create(first_name: FFaker::Name.first_name, last_name: FFaker::Name.last_name, known_for: FFaker::Movie.title)
-end
+# 10.times do 
+#   actor = Actor.create(first_name: FFaker::Name.first_name, last_name: FFaker::Name.last_name, known_for: FFaker::Movie.title)
+# end
+
+actor = Actor.create(first_name: "John", last_name: "Travolta", known_for: "Saturday Night Fever", gender: "Male", age: 68, movie_id: 2)
+
+actor = Actor.create(first_name: "Samuel", last_name: "Jackson", known_for: "Snakes on a Plane", gender: "Male", age: 73, movie_id: 2)
+
+actor = Actor.create(first_name: "Uma", last_name: "Thurman", known_for: "Kill Bill", gender: "Female", age: 51, movie_id: 2)
 
 # Movie.create(title: "No Country for Old Men", year: 2007, plot:"Violence and mayhem ensue after a hunter stumbles upon a drug deal gone wrong and more than two million dollars in cash near the Rio Grande.")
 
