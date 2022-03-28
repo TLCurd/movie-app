3c0d3cfd3155d6879aaf35810c079ff7acfee7bb
TLCurd <TLCurdChiro@gmail.com>
2022-03-27 22:00:08 -0600

    adds genre_names method and updates show/index templates

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -5,4 +5,16 @@ class Movie < ApplicationRecord
   has_many :actors
   has_many :movie_genres
   has_many :genres, through: :movie_genres
+
+  def genre_names
+    all = []
+    genres.each do |genre|
+      all << genre[:name]
+    end
+    return all
+    # movie.genres.each do |genre|
+    #   genres << genre[:name] 
+    # end
+   
+  end
 end
