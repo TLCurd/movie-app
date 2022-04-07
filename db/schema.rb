6119928d65c3a6a7483bd15e93d257cd83945aae
TLCurd <TLCurdChiro@gmail.com>
2022-04-06 21:39:13 -0600

    add image_url to movies

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_28_022455) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_07_033731) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -46,6 +46,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_28_022455) do
     t.datetime "updated_at", null: false
     t.string "director"
     t.boolean "english", default: true
+    t.string "image_url"
   end
 
   create_table "users", force: :cascade do |t|
