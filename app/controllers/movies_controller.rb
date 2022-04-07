e0ce1ddbde69b386ddac3ff2e96f7082d8a70415
TLCurd <TLCurdChiro@gmail.com>
2022-04-06 23:03:09 -0600

    remove genre param in movie create to help w/ create action in Vue file (issues w/ associations - FIX LATER)

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,7 +7,7 @@ class MoviesController < ApplicationController
   end
 
   def create
-    movie = Movie.new(title: params[:title], year: params[:year], plot: params[:plot], director: params[:director], genre: [params[:genre]])
+    movie = Movie.new(title: params[:title], year: params[:year], plot: params[:plot], director: params[:director])
     if movie.save
       render json: movie.as_json
     else
